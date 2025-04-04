c1c02576b9db1bf488ed5e44c9859d187addb7b7
darrasHumber <n01546480@humber.ca>
2025-04-04 19:39:44 -0400

    Complete Part 2

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Part 1
 console.log("Understanding and Creating Objects\n\n");
+
+//Create an object to represent a student
 const darras = {
   name: "Mohammed Darras",
   age: 30,
@@ -14,8 +16,20 @@ const darras = {
   },
 };
 
+//Use console.log to output, Name age and displayInfo output
 console.log("Name", darras.name);
 console.log("Age", darras.age);
 darras.displayInfo();
 
+console.log("\n================================================");
+// Part 2
+console.log("Working with JSON");
+// Convert the student object into a JSON strin
+const darrasJsonString = JSON.stringify(darras, null, 2);
+// Log the JSON string to the console.
+console.log("JSON String \n", darrasJsonString);
+//Convert the JSON string back into a JavaScript object
+const darrasObj = JSON.parse(darrasJsonString);
+//Log the newly created object and compare it to the original.
+console.log("JSON object", darrasObj);
 console.log("\n================================================");
